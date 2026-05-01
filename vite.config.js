430fa3301846ae1d2ddb635d0942bf2763c29c63
tung <nguyenthanhtungn5@gmail.com>
2026-05-01 22:17:25 +0200

    Config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default {
-  server: {
-    host: "0.0.0.0",
-    port: 3001,
-    allowedHosts: ["www.auzora.de", "auzora.de"],
-  },
-};
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+
+  return {
+    plugins: [react()],
+
+    server: {
+      host: "0.0.0.0",
+      port: 3001,
+
+      // nur für production domain nötig
+      allowedHosts: isDev ? true : ["www.auzora.de", "auzora.de"],
+
+      // proxy nur in dev!
+      ...(isDev && {
+        proxy: {
+          "/api": {
+            target: "http://localhost:8080",
+            changeOrigin: true,
+          },
+        },
+      }),
+    },
+  };
+});
